43966ea3ae12a77312d664b1aaedc13ab341f0e7
derekChowProcarrier <derek.chow@pro-carrier.com>
2024-06-07 07:37:38 +0800

    update backprop

--- backprop.go
+++ backprop.go
@@ -19,40 +19,42 @@ func cost(predict []float32, target []float32) []float32 {
 // SDG is a function for SDG optimizer
 // input is list of data, target is list of target
 // len of inputs and targets must be the same
-func (c *Chain) Update(inputs, targets [][]float32) {
+func (c *Chain) Update(inputs, targets [][]float32) { //target [1]float32  input [16]float32
 	// placeholder
 	// check len of input and target
 	if len(inputs) == 0 || len(targets) == 0 {
 		return
 	}
-
+	var predictZ []float32
 	for i, input := range inputs {
-		predict, err := c.Predict(input)
+		predict, err := c.Predict(input)         //predict [1]float32
+		predictZ = (*c.Layers)[i].Prime(predict) //predictZ [1]float32
 		if err != nil && len(predict) != len(targets[i]) && len(predict) != len(*(*c.Layers)[len(*c.Layers)-1].Neurons) {
 			return
 		}
-	}
+		var Cost []float32
+		// calculate cost
+		Cost = cost(predict, targets[i])
+		// calculate gradient
+		for i := len(*c.Layers) - 1; i >= 0; i-- {
 
+		}
+	}
 }
 
-func (c *Chain) SDG(predict, target []float32) {
-	var Cost []float32
-	// calculate cost
-	Cost = cost(predict, target)
-	// calculate gradient
-	for i := len(*c.Layers) - 1; i >= 0; i-- {
-		z := (*c.Layers)[i].Prime(Cost)
-		for j, neuron := range *(*c.Layers)[i].Neurons {
-			dLossDYPred := 2.0 * (z[j] - target[j])
-			for i, weight := range *neuron.Weights {
-				// Partial derivative of loss with respect to weight (dLoss/dW)
-				//gradW[i] = dLossDYPred * input[i]
+func (c *Chain) SDG(predict, thisZ []float32) {
 
-				// Update the weight
-				weight += (*c.Layers)[i].LearningRate * dLossDYPred * target[j]
-				// n.weights[i] += learningRate * gradient * input[i]
-			}
-			(*(*c.Layers)[i].Bias)[j] += (*c.Layers)[i].LearningRate * dLossDYPred
+	for j, neuron := range *(*c.Layers)[i].Neurons {
+		dLossDYPred := 2.0 * (z[j] - target[j])
+		for i, weight := range *neuron.Weights {
+			// Partial derivative of loss with respect to weight (dLoss/dW)
+			//gradW[i] = dLossDYPred * input[i]
+
+			// Update the weight
+			weight += (*c.Layers)[i].LearningRate * dLossDYPred * target[j]
+			// n.weights[i] += learningRate * gradient * input[i]
 		}
+		(*(*c.Layers)[i].Bias)[j] += (*c.Layers)[i].LearningRate * dLossDYPred
+
 	}
 }
